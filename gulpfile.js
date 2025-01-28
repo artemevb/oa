ac3134ae1ac05c038e90f003e6d8200c25186ae2
Andrey <aa71555443@gmail.com>
2025-01-28 17:54:01 +0500

    new project

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+
+function htmlTask() {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+// CSS task
+function cssTask() {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([tailwindcss(), autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+// JavaScript task
+function jsTask() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist/js'));
+}
+
+// Image task
+function imageTask() {
+    return gulp.src('./src/images/**/*')
+        .pipe(gulp.dest('./dist/images'))
+        .on('end', () => console.log('Images copied!'));
+}
+
+// Watch task
+function watchTask() {
+    gulp.watch('./src/css/**/*.css', cssTask);
+    gulp.watch('./src/js/**/*.js', jsTask);
+    gulp.watch('./src/images/**/*', imageTask);
+    gulp.watch('./src/**/*.html', htmlTask);
+}
+
+// Default Gulp task
+exports.default = gulp.series(
+    gulp.parallel(cssTask, jsTask, imageTask, htmlTask),
+    watchTask
+);
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#0EB0AA",
+        mainTheme: "#F5F5F5"
+      },
+    },
+  },
+  plugins: [],
+}
+
